a54870f1c541c477df044529d169cd05d8c50a00
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-01-14 14:26:04 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  return (
+    <body>
+      <Box id="signInCard" color='primary'>
+        <p>Sign In</p>
+        <hr></hr>
+        <TextField label="Enter your username" />
+        <br></br>
+        <br></br>
+        <TextField label="Enter your password" />
+        <br></br>
+        <br></br>
+        <Button variant='contained' color='primary'>
+          Submit
+        </Button>
+      </Box>
+    </body>
+  );
+}
+
+export default App;
